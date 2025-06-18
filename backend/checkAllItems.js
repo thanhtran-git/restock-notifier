83d3e3873691e5b56a1143ec512dd76014e16a59
thanhtran-git <thanh.tran@live.de>
2025-06-18 21:41:05 +0200

    move cron to server.js, add port

--- backend/checkAllItems.js
+++ backend/checkAllItems.js
@@ -1,13 +1,9 @@
 import { checkStock } from "./checkStock.js";
 import { ITEMS_TO_MONITOR } from "./config.js";
-import cron from "node-cron";
 
-async function checkAllItems() {
+export async function checkAllItems() {
   console.log("🔄 Starting stock check for all items...");
   for (const item of ITEMS_TO_MONITOR) {
     await checkStock(item);
   }
 }
-
-cron.schedule("0 * * * *", checkAllItems);
-checkAllItems();
---
+++ server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { checkAllItems } from "./backend/checkAllItems.js";
+import cron from "node-cron";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (_req, res) => {
+  res.send("Voostore Restock Notifier is running");
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Server running on port ${port}`);
+  checkAllItems();
+  cron.schedule("*/10 * * * *", checkAllItems);
+});
